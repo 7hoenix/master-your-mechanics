b06abe82967c017cf039d2f6dc473ed4d909ed8f
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-08-18 08:35:12 -0600

    Testing brightflash from email for security

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "Spamming You <john@johnholzmann.com>"
+	default from: "Spamming You <brightflash@gmail.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
