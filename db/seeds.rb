b0e11338e3364f6e79d195828acf9af646228d56
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-09-03 11:41:31 -0600

    Added Master Your Mechanics course to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+master_your_mechanics = Product.create(title: "Master Your Mechanics",
+	author: "Mike Miller", price: "4.99", sku: "MASTAURMECHANICS1", description: %{<p>Do you want to finally hit gold this season? What about plat or even diamond? If you want to make it to the top you need to start with the most important aspect of the game: mechanics.</p>
+
+            <p>You can't control very much in League of Legends. You can't stop your team mates from feeding, disconnecting, or flaming. The one thing you can control is how you control your character and influence your team with proper decision making.</p>
+
+            <p>And let's face it. . . its a LOT easier to get your team to listen to you when you have 6 kills 12 minutes into the game.</p>
+
+            <p><strong>So wait who are you and why should I listen to you?</strong></p>
+            
+            <p>My name is Mike Miller and three weeks ago I was Silver II on my main. I am now in my Platinum promos and I'm still carrying like its Silver.</p>
+            
+            <p>Full Disclosure: I've been playing League of Legends since season one. I have probably over a thousand normal games played and I have watched a ton of streams at work. I didn't invent the technology from the Matrix to speed learn how to play with Diamond mechanics in a week. No no no. . .</p>
+
+            <p><strong>I had to work for it</strong></p>
+
+            <p>League of Legends did not come easily for me. I wasn't one of the "Pros" you see that played for 6 months and was suddenly a challenger level player.</p>
+
+            <p>I finally made it to Gold in season three using gimmicky strategies (like teleport with homeguard boots on Ryze and constantly ganking bot lane like a bat out of hell). When the cheese strategies worked I would win handily. When they didn't I would lose. Hard.</p>})
