42aebaff16bfb761352669541ebb4c21dc79c07e
philmphoenix <phillip.matthew.phoenix@gmail.com>
2014-11-23 19:33:29 -0700

    Made a few changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 master_your_mechanics = Product.create(title: "Master Your",
-	subtitle: "Mechanics", author: "Phillip Matthew Phoenix", length: "65 Pages of Awesome", author_image_name: "teacher-image.jpg", price: "4.99", sku: "MASTAURMECHANICS1", download_url: "https://s3.amazonaws.com/Master-Your-Mechanics/Practice+Like+a+Pro+1.0.pdf", details: "One ebook", description: %{<p>Do you want to finally hit gold this season? What about plat or even diamond? If you want to make it to the top you need to start with the most important aspect of the game: mechanics.</p>
+	subtitle: "Mechanics", author: "Phillip Matthew Phoenix", length: "65 Pages of Awesome", author_image_name: "teacher-image.png", price: "4.99", sku: "MASTAURMECHANICS1", download_url: "https://s3.amazonaws.com/Master-Your-Mechanics/Practice+Like+a+Pro+1.0.pdf", details: "One ebook",
+	description: %{<p>Do you want to finally hit gold this season? What about plat or even diamond? If you want to make it to the top you need to start with the most important aspect of the game: mechanics.</p>
 
             <p>You can't control very much in League of Legends. You can't stop your team mates from feeding, disconnecting, or flaming. The one thing you can control is how you control your character and influence your team with proper decision making.</p>
 
@@ -23,7 +24,8 @@ master_your_mechanics = Product.create(title: "Master Your",
 
             <p>League of Legends did not come easily for me. I wasn't one of the "Pros" you see that played for 6 months and was suddenly a challenger level player.</p>
 
-            <p>I finally made it to Gold in season three using gimmicky strategies (like teleport with homeguard boots on Ryze and constantly ganking bot lane like a bat out of hell). When the cheese strategies worked I would win handily. When they didn't I would lose. Hard.</p>}, author_description: %{<p>Hey, I'm Mike Miller and three weeks ago I was Silver II on my main. I am now in Platinum and I'm still carrying like its Silver.</p>
+            <p>I finally made it to Gold in season three using gimmicky strategies (like teleport with homeguard boots on Ryze and constantly ganking bot lane like a bat out of hell). When the cheese strategies worked I would win handily. When they didn't I would lose. Hard.</p>},
+						author_description: %{<p>Hey, I'm Mike Miller and three weeks ago I was Silver II on my main. I am now in Platinum and I'm still carrying like its Silver.</p>
                 <p>Full Disclosure: I've been playing League of Legends since season one. I have probably over a thousand normal games played and I have watched a ton of streams at work. I didn't invent the technology from the Matrix to speed learn how to play with Diamond mechanics in a week. No no no. . .
                 </p>
                 <p><strong>I had to work for it</strong></p>
